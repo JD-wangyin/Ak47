c911b467cac87c3f20d964eeba00b57e45dcc09f
hannyu <hannyu@gmail.com>
2015-07-16 11:03:37 +0800

    add more javadocs

--- pipe-http/src/main/java/com/wangyin/ak47/pipes/http/SimpleHttpResponse.java
+++ pipe-http/src/main/java/com/wangyin/ak47/pipes/http/SimpleHttpResponse.java
@@ -1,6 +1,5 @@
 package com.wangyin.ak47.pipes.http;
 
-
 import com.wangyin.ak47.common.Ak47Constants;
 import com.wangyin.ak47.common.Logger;
 
@@ -27,56 +26,102 @@ public class SimpleHttpResponse extends SimpleHttpMessage {
     private String statusCode = "200";
     private String reasonPhrase = "OK";
     
-
+    /**
+     * A Simple Http Response with default params.
+     */
     public SimpleHttpResponse(){
         this(null, null, null);
         addHeader("Server", Ak47Constants.NAME+"/"+Ak47Constants.VERSION);
     }
+    
+    /**
+     * A Simple Http Response with given statusCode.
+     * 
+     * @param statusCode        Status code, such as 200
+     */
     public SimpleHttpResponse(String statusCode){
-        this(null, statusCode, null);
+        this(statusCode, null, null);
     }
+    
+    /**
+     * A Simple Http Response with given statusCode and reasonPhrase.
+     * 
+     * @param statusCode        Status code, such as 200
+     * @param reasonPhrase      Reason phrase
+     */
     public SimpleHttpResponse(String statusCode, String reasonPhrase){
-        this(null, statusCode, reasonPhrase);
-    }
-    public SimpleHttpResponse(String httpVersion, String statusCode, String reasonPhrase){
-        setResponseLine(httpVersion, statusCode, reasonPhrase);
+        this(statusCode, reasonPhrase, null);
     }
     
+    /**
+     * A Simple Http Response with given statusCode, reasonPhrase and httpVersion.
+     * 
+     * @param statusCode        Status code, such as 200
+     * @param reasonPhrase      Reason phrase, such as OK
+     * @param httpVersion       Http version
+     */
+    public SimpleHttpResponse(String statusCode, String reasonPhrase, String httpVersion){
+        setResponseLine(statusCode, reasonPhrase, httpVersion);
+    }
     
-
+    /**
+     * 
+     * @return          Http version
+     */
     public String getHttpVersion() {
         return httpVersion;
     }
+    /**
+     * 
+     * @param httpVersion   Http version
+     */
     public void setHttpVersion(String httpVersion) {
         this.httpVersion = httpVersion;
         updateStartLine();
     }
+    /**
+     * 
+     * @return              Status code, such as 200
+     */
     public String getStatusCode() {
         return statusCode;
     }
+    /**
+     * 
+     * @param statusCode    Status code, such as 200
+     */
     public void setStatusCode(String statusCode) {
         this.statusCode = statusCode;
         updateStartLine();
     }
+    /**
+     * 
+     * @return              Reason phrase, such as OK
+     */
     public String getReasonPhrase() {
         return reasonPhrase;
     }
+    /**
+     * 
+     * @param reasonPhrase  Reason phrase, such as OK
+     */
     public void setReasonPhrase(String reasonPhrase) {
         this.reasonPhrase = reasonPhrase;
         updateStartLine();
     }
     
+    
     /**
-     * 设置 第一行
+     * set Response-Line
      * 
-     * @param method
-     * @param url
-     * @param httpVersion
+     * @param statusCode        Status code, such as 200
+     * @param reasonPhrase      Reason phrase, such as OK
+     * @param httpVersion       Http version
      */
-    public void setResponseLine(String httpVersion, String statusCode, String reasonPhrase){
-        this.httpVersion = httpVersion==null?this.httpVersion:httpVersion;
+    public void setResponseLine(String statusCode, String reasonPhrase, String httpVersion){
         this.statusCode = statusCode==null?this.statusCode:statusCode;
         this.reasonPhrase = reasonPhrase==null?this.reasonPhrase:reasonPhrase;
+        this.httpVersion = httpVersion==null?this.httpVersion:httpVersion;
         updateStartLine();
     }
     
@@ -87,8 +132,8 @@ public class SimpleHttpResponse extends SimpleHttpMessage {
     /**
      * 转化，如果格式不对则返回null
      * 
-     * @param shm
-     * @return
+     * @param shm       SimpleHttpMessage
+     * @return          HttpResponse or null if Response-Line malformed 
      */
     public static SimpleHttpResponse valueOf(SimpleHttpMessage shm){
       String startLine = shm.getStartLine();
@@ -108,10 +153,17 @@ public class SimpleHttpResponse extends SimpleHttpMessage {
     }
 
     
+    /**
+     * 
+     * Deep copy with Response-Line, headers and body.
+     * 
+     * @param response      HttpResponse
+     */
     public void copyOf(SimpleHttpResponse response){
-        this.setResponseLine(response.getHttpVersion(), response.getStatusCode(), 
-                response.getReasonPhrase());
+        this.setResponseLine(response.getStatusCode(), response.getReasonPhrase(), 
+                response.getHttpVersion());
         this.setHeaders(response.getHeaders());
         this.setContent(response.getContent());
     }
+    
 }
